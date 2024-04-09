38b3c69b3df4df3e7dc107a39b9e3b6f6004b8db
Beyza Keser <beyzaaakeser@gmail.com>
2024-04-09 16:39:08 +0300

    js codes

--- main.js
+++ main.js
@@ -1,57 +1,61 @@
 //! Gerekli HTML elementlerini seç
-const form = document.querySelector(".grocery-form");
-const grocery = document.getElementById("grocery");
-const container = document.querySelector(".grocery-container");
-const list = document.querySelector(".grocery-list");
-const alert = document.querySelector(".alert");
-const submitBtn = document.querySelector(".submit-btn");
-const clearBtn = document.querySelector(".clear-btn");
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const alert = document.querySelector('.alert');
+const submitBtn = document.querySelector('.submit-btn');
+const clearBtn = document.querySelector('.clear-btn');
 
 //* Düzenleme Seçenekleri
 let editElement;
 let editFlag = false; // Düzenleme modunda olup olmadığını belirtir.
-let editID = ""; // Düzenleme yapılan ögenin benzersiz kimliği
+let editID = ''; // Düzenleme yapılan ögenin benzersiz kimliği
 
-//Fonksiyonlar 
+//Fonksiyonlar
 
-const displayAlert = (text,action) =>{
-    alert.textContent = text;
-    alert.classList.add(`alert-${action}`);
-    setTimeout(()=>{
-        alert.textContent = "";
-        alert.classList.remove(`alert-${action}`)
-    },2000)
-}
+const displayAlert = (text, action) => {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 2000);
+};
 
 // Tikladiginiz article etiketini ekrandan kaldiracak fonksiyon
-const deleteItem = (e)=>{
+const deleteItem = (e) => {
   const element = e.currentTarget.parentElement.parentElement; // article etiketine eristik.
   const id = element.dataset.id;
   list.removeChild(element); // list etiketi icerisinden article etiketini kaldirdik.
-  displayAlert("Note deleted successfully","danger")
-}
-
+  displayAlert('Note deleted successfully', 'danger');
+};
 
 const editItem = (e) => {
   const element = e.currentTarget.parentElement.parentElement; // article etiketine, parentElement kullanarak eristik.
-  
-  
-}
+  editElement = e.currentTarget.parentElement.previousElementSibling; // butonun kapsayicisina eristikten sonra kapsayicinin kardes elementine eristik.
+  // tikladigim article etiketi icerisindeki p etiketinin textini, inputun icerisine gonder
+  grocery.value = editElement.innerText;
+  editFlag = true; // duzenleme modu
+  editID  = element.dataset.id; // duzenlenen ogenin Id'sine eristik ve globaldeki editID'ye esitledik.
+  submitBtn.textContent = 'Edit'; // butonun textini duzenle olarak degistirdik.
 
-const addItem = (e) =>{
-    e.preventDefault(); // formun otomaik olarak gonderilmesini engeller
-    const value = grocery.value; // form icerisinde bulunan inputun degerini aldik.
-    const id = new Date().getTime().toString(); // benzersiz bir id olusturduk.
-    
-    // Eger input bos degilse ve duzenleme modunda degilse calisacak blog yapisi
-    if(value !== "" && !editFlag){
-        const element = document.createElement("article"); // Yeni bir article etiketi olusturduk
-        let attr = document.createAttribute("data-id"); // article etiketine data-id attribute'u olusturduk. Yeni bir veri kimligi olusturduk.
-        attr.value = id; // attribute'un degerini id'ye esitledik
-        element.setAttributeNode(attr); // article etiketine data-id attribute'u setledik ekledik.
-        element.classList.add("grocery-item"); // article etiketine class ekledik.  
+};
 
-        element.innerHTML = `
+const addItem = (e) => {
+  e.preventDefault(); // formun otomaik olarak gonderilmesini engeller
+  const value = grocery.value; // form icerisinde bulunan inputun degerini aldik.
+  const id = new Date().getTime().toString(); // benzersiz bir id olusturduk.
+
+  // Eger input bos degilse ve duzenleme modunda degilse calisacak blog yapisi
+  if (value !== '' && !editFlag) {
+    const element = document.createElement('article'); // Yeni bir article etiketi olusturduk
+    let attr = document.createAttribute('data-id'); // article etiketine data-id attribute'u olusturduk. Yeni bir veri kimligi olusturduk.
+    attr.value = id; // attribute'un degerini id'ye esitledik
+    element.setAttributeNode(attr); // article etiketine data-id attribute'u setledik ekledik.
+    element.classList.add('grocery-item'); // article etiketine class ekledik.
+
+    element.innerHTML = `
         
         <p class="title">${value}</p>
             <div class="btn-container">
@@ -62,20 +66,17 @@ const addItem = (e) =>{
                 <i class="fa-solid fa-trash"></i>
               </button>
             </div>
-        `
+        `;
 
-        const deleteBtn = element.querySelector(".delete-btn");
-        deleteBtn.addEventListener("click",deleteItem);
-        const editBtn = element.querySelector("edit-btn");
-        editBtn.addEventListener("click",editItem);
-        list.appendChild(element); // list'e article etiketini ekledik.
-        displayAlert("Added Successfully","success")
-        container.classList.add("show-container")
-    }
-
-}
-
-
+    const deleteBtn = element.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', deleteItem);
+    const editBtn = element.querySelector('.edit-btn');
+    editBtn.addEventListener('click', editItem);
+    list.appendChild(element); // list'e article etiketini ekledik.
+    displayAlert('Note added successfully', 'success');
+    container.classList.add('show-container');
+  }
+};
 
 // Olay Izleyicileri
-form.addEventListener("submit", addItem);
+form.addEventListener('submit', addItem);
